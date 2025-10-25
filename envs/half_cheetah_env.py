69c2dd4558c1a2fe8c24472e39aa90ce6f5a353c
Isoceanum <Isoceanum@outlook.com>
2025-10-25 15:00:20 +0200

    add half_cheetah

---
+++ envs/half_cheetah_env.py
@@ -0,0 +1,323 @@
+__credits__ = ["Rushiv Arora"]
+
+import numpy as np
+from pathlib import Path
+
+from gymnasium.envs.mujoco import MujocoEnv
+from gymnasium.spaces import Box
+from gymnasium.utils.ezpickle import EzPickle
+
+DEFAULT_CAMERA_CONFIG = {
+    "distance": 4.0,
+}
+
+XML_PATH = str(Path(__file__).with_name("assets") / "half_cheetah.xml")
+
+
+class HalfCheetahRewardFn:
+    """Pickleable reward callable used by the planner in vectorized setups."""
+
+    def __init__(self, dt, forward_reward_weight, ctrl_cost_weight):
+        self.dt = float(dt)
+        self.forward_reward_weight = float(forward_reward_weight)
+        self.ctrl_cost_weight = float(ctrl_cost_weight)
+
+    def __call__(self, state, next_state, action):
+        import torch
+
+        com_x_before = state[:, -3]
+        com_x_after = next_state[:, -3]
+        com_x_velocity = (com_x_after - com_x_before) / self.dt
+        forward_reward = self.forward_reward_weight * com_x_velocity
+        ctrl_cost = self.ctrl_cost_weight * torch.sum(action ** 2, dim=-1)
+        return forward_reward - ctrl_cost
+
+
+class HalfCheetahTerminationFn:
+    def __call__(self, next_state):
+        import torch
+
+        return torch.zeros(next_state.shape[0], dtype=torch.bool, device=next_state.device)
+
+
+class HalfCheetahEnv(MujocoEnv, EzPickle):
+    """
+    ### Description
+
+    This environment is based on the work by P. Wawrzyński in
+    ["A Cat-Like Robot Real-Time Learning to Run"](http://staff.elka.pw.edu.pl/~pwawrzyn/pub-s/0812_LSCLRR.pdf).
+    The HalfCheetah is a 2-dimensional robot consisting of 9 links and 8
+    joints connecting them (including two paws). The goal is to apply a torque
+    on the joints to make the cheetah run forward (right) as fast as possible,
+    with a positive reward allocated based on the distance moved forward and a
+    negative reward allocated for moving backward. The torso and head of the
+    cheetah are fixed, and the torque can only be applied on the other 6 joints
+    over the front and back thighs (connecting to the torso), shins
+    (connecting to the thighs) and feet (connecting to the shins).
+
+    ### Action Space
+    The action space is a `Box(-1, 1, (6,), float32)`. An action represents the torques applied between *links*.
+
+    | Num | Action                                  | Control Min | Control Max | Name (in corresponding XML file) | Joint | Unit         |
+    | --- | --------------------------------------- | ----------- | ----------- | -------------------------------- | ----- | ------------ |
+    | 0   | Torque applied on the back thigh rotor  | -1          | 1           | bthigh                           | hinge | torque (N m) |
+    | 1   | Torque applied on the back shin rotor   | -1          | 1           | bshin                            | hinge | torque (N m) |
+    | 2   | Torque applied on the back foot rotor   | -1          | 1           | bfoot                            | hinge | torque (N m) |
+    | 3   | Torque applied on the front thigh rotor | -1          | 1           | fthigh                           | hinge | torque (N m) |
+    | 4   | Torque applied on the front shin rotor  | -1          | 1           | fshin                            | hinge | torque (N m) |
+    | 5   | Torque applied on the front foot rotor  | -1          | 1           | ffoot                            | hinge | torque (N m) |
+
+
+    ### Observation Space
+
+    Observations consist of positional values of different body parts of the
+    cheetah, followed by the velocities of those individual parts (their derivatives) with all the positions ordered before all the velocities.
+
+    By default, observations do not include the x-coordinate of the cheetah's center of mass. It may
+    be included by passing `exclude_current_positions_from_observation=False` during construction.
+    In that case, the observation space will have 18 dimensions where the first dimension
+    represents the x-coordinate of the cheetah's center of mass.
+    Regardless of whether `exclude_current_positions_from_observation` was set to true or false, the x-coordinate
+    will be returned in `info` with key `"x_position"`.
+
+    However, by default, the observation is a `ndarray` with shape `(17,)` where the elements correspond to the following:
+
+
+    | Num | Observation                          | Min  | Max | Name (in corresponding XML file) | Joint | Unit                     |
+    | --- | ------------------------------------ | ---- | --- | -------------------------------- | ----- | ------------------------ |
+    | 0   | z-coordinate of the front tip        | -Inf | Inf | rootz                            | slide | position (m)             |
+    | 1   | angle of the front tip               | -Inf | Inf | rooty                            | hinge | angle (rad)              |
+    | 2   | angle of the second rotor            | -Inf | Inf | bthigh                           | hinge | angle (rad)              |
+    | 3   | angle of the second rotor            | -Inf | Inf | bshin                            | hinge | angle (rad)              |
+    | 4   | velocity of the tip along the x-axis | -Inf | Inf | bfoot                            | hinge | angle (rad)              |
+    | 5   | velocity of the tip along the y-axis | -Inf | Inf | fthigh                           | hinge | angle (rad)              |
+    | 6   | angular velocity of front tip        | -Inf | Inf | fshin                            | hinge | angle (rad)              |
+    | 7   | angular velocity of second rotor     | -Inf | Inf | ffoot                            | hinge | angle (rad)              |
+    | 8   | x-coordinate of the front tip        | -Inf | Inf | rootx                            | slide | velocity (m/s)           |
+    | 9   | y-coordinate of the front tip        | -Inf | Inf | rootz                            | slide | velocity (m/s)           |
+    | 10  | angle of the front tip               | -Inf | Inf | rooty                            | hinge | angular velocity (rad/s) |
+    | 11  | angle of the second rotor            | -Inf | Inf | bthigh                           | hinge | angular velocity (rad/s) |
+    | 12  | angle of the second rotor            | -Inf | Inf | bshin                            | hinge | angular velocity (rad/s) |
+    | 13  | velocity of the tip along the x-axis | -Inf | Inf | bfoot                            | hinge | angular velocity (rad/s) |
+    | 14  | velocity of the tip along the y-axis | -Inf | Inf | fthigh                           | hinge | angular velocity (rad/s) |
+    | 15  | angular velocity of front tip        | -Inf | Inf | fshin                            | hinge | angular velocity (rad/s) |
+    | 16  | angular velocity of second rotor     | -Inf | Inf | ffoot                            | hinge | angular velocity (rad/s) |
+
+    ### Rewards
+    The reward consists of two parts:
+    - *forward_reward*: A reward of moving forward which is measured
+    as *`forward_reward_weight` * (x-coordinate before action - x-coordinate after action)/dt*. *dt* is
+    the time between actions and is dependent on the frame_skip parameter
+    (fixed to 5), where the frametime is 0.01 - making the
+    default *dt = 5 * 0.01 = 0.05*. This reward would be positive if the cheetah
+    runs forward (right).
+    - *ctrl_cost*: A cost for penalising the cheetah if it takes
+    actions that are too large. It is measured as *`ctrl_cost_weight` *
+    sum(action<sup>2</sup>)* where *`ctrl_cost_weight`* is a parameter set for the
+    control and has a default value of 0.1
+
+    The total reward returned is ***reward*** *=* *forward_reward - ctrl_cost* and `info` will also contain the individual reward terms
+
+    ### Starting State
+    All observations start in state (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,) with a noise added to the
+    initial state for stochasticity. As seen before, the first 8 values in the
+    state are positional and the last 9 values are velocity. A uniform noise in
+    the range of [-`reset_noise_scale`, `reset_noise_scale`] is added to the positional values while a standard
+    normal noise with a mean of 0 and standard deviation of `reset_noise_scale` is added to the
+    initial velocity values of all zeros.
+
+    ### Episode End
+    The episode truncates when the episode length is greater than 1000.
+
+    ### Arguments
+
+    No additional arguments are currently supported in v2 and lower.
+
+    ```
+    env = gym.make('HalfCheetah-v2')
+    ```
+
+    v3 and v4 take gym.make kwargs such as xml_file, ctrl_cost_weight, reset_noise_scale etc.
+
+    ```
+    env = gym.make('HalfCheetah-v4', ctrl_cost_weight=0.1, ....)
+    ```
+
+    | Parameter                                    | Type      | Default              | Description                                                                                                                                                       |
+    | -------------------------------------------- | --------- | -------------------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+    | `xml_file`                                   | **str**   | `"half_cheetah.xml"` | Path to a MuJoCo model                                                                                                                                            |
+    | `forward_reward_weight`                      | **float** | `1.0`                | Weight for _forward_reward_ term (see section on reward)                                                                                                          |
+    | `ctrl_cost_weight`                           | **float** | `0.1`                | Weight for _ctrl_cost_ weight (see section on reward)                                                                                                             |
+    | `reset_noise_scale`                          | **float** | `0.1`                | Scale of random perturbations of initial position and velocity (see section on Starting State)                                                                    |
+    | `exclude_current_positions_from_observation` | **bool**  | `True`               | Whether or not to omit the x-coordinate from observations. Excluding the position can serve as an inductive bias to induce position-agnostic behavior in policies |
+
+    ### Version History
+
+    * v4: all mujoco environments now use the mujoco bindings in mujoco>=2.1.3
+    * v3: support for gym.make kwargs such as xml_file, ctrl_cost_weight, reset_noise_scale etc. rgb rendering comes from tracking camera (so agent does not run away from screen)
+    * v2: All continuous control environments now use mujoco_py >= 1.50
+    * v1: max_time_steps raised to 1000 for robot based tasks. Added reward_threshold to environments.
+    * v0: Initial versions release (1.0.0)
+    """
+
+    metadata = {
+        "render_modes": [
+            "human",
+            "rgb_array",
+            "depth_array",
+        ],
+        "render_fps": 20,
+    }
+
+    def __init__(
+        self,
+        forward_reward_weight=1.0,
+        ctrl_cost_weight=0.05,
+        reset_noise_scale=0.1,
+        exclude_current_positions_from_observation=True,
+        **kwargs
+    ):
+        EzPickle.__init__(
+            self,
+            forward_reward_weight,
+            ctrl_cost_weight,
+            reset_noise_scale,
+            exclude_current_positions_from_observation,
+            **kwargs
+        )
+
+        self._forward_reward_weight = forward_reward_weight
+
+        self._ctrl_cost_weight = ctrl_cost_weight
+
+        self._reset_noise_scale = reset_noise_scale
+
+        self._exclude_current_positions_from_observation = (
+            exclude_current_positions_from_observation
+        )
+
+        # Append torso COM (x,y,z) to observation to match Nagabandi env
+        # Always exclude root x from observations: obs = qpos[1:], qvel, torso COM (3)
+        # => 17 (qpos excluding root x) + 17 (qvel) + 3 (COM) = 37? but classic HalfCheetah has 17 total without COM
+        # Gymnasium HalfCheetah uses 17 dims when excluding root x (qpos[1:] + qvel) -> 17
+        # We add 3 COM dims -> 20 total
+        observation_space = Box(low=-np.inf, high=np.inf, shape=(20,), dtype=np.float64)
+
+        MujocoEnv.__init__(
+            self, XML_PATH, 5, observation_space=observation_space, **kwargs
+        )
+
+    def control_cost(self, action):
+        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
+        return control_cost
+
+    def step(self, action):
+        # Root x position (kept in info for continuity)
+        x_position_before = self.data.qpos[0]
+        # Torso COM x position before
+        com_before = self._get_torso_com()[0]
+
+        self.do_simulation(action, self.frame_skip)
+
+        x_position_after = self.data.qpos[0]
+        x_velocity = (x_position_after - x_position_before) / self.dt
+
+        # Torso COM x velocity for forward reward (Nagabandi semantics)
+        com_after = self._get_torso_com()[0]
+        com_x_velocity = (com_after - com_before) / self.dt
+
+        ctrl_cost = self.control_cost(action)
+
+        forward_reward = self._forward_reward_weight * com_x_velocity
+
+        observation = self._get_obs()
+        reward = forward_reward - ctrl_cost
+        terminated = False
+        info = {
+            "x_position": x_position_after,   # root body position (for compatibility)
+            "x_velocity": x_velocity,        # root body velocity
+            "com_x_position": com_after,     # torso COM position (x)
+            "com_x_velocity": com_x_velocity,
+            "reward_run": forward_reward,
+            "reward_ctrl": -ctrl_cost,
+        }
+
+        if self.render_mode == "human":
+            self.render()
+        return observation, reward, terminated, False, info
+
+    def _get_obs(self):
+        position = self.data.qpos.flat.copy()
+        velocity = self.data.qvel.flat.copy()
+        # Always exclude root x to match Nagabandi
+        position = position[1:]
+
+        # Append torso COM (x, y, z) as last 3 entries to match original code
+        com = self._get_torso_com().copy()
+
+        observation = np.concatenate((position, velocity, com)).ravel()
+        return observation
+
+    def _get_torso_com(self):
+        """Return torso COM (x,y,z) as numpy array, robust across Mujoco bindings."""
+        try:
+            # Try new mujoco (gymnasium) fields first
+            # body_names is available; prefer name2id if present
+            try:
+                idx = self.model.body_names.index("torso")
+            except Exception:
+                try:
+                    idx = self.model.body_name2id("torso")
+                except Exception:
+                    idx = 0
+
+            if hasattr(self.data, "subtree_com"):
+                return self.data.subtree_com[idx]
+            if hasattr(self.data, "com_subtree"):
+                return self.data.com_subtree[idx]
+            # Fallback: body origin position (not exact COM but reasonable fallback)
+            if hasattr(self.data, "xipos"):
+                return self.data.xipos[idx]
+            if hasattr(self.data, "body") and hasattr(self.data.body, "xpos"):
+                return self.data.body.xpos[idx]
+        except Exception:
+            pass
+        # Last resort: zeros
+        return np.zeros(3, dtype=np.float64)
+
+    def reset_model(self):
+        noise_low = -self._reset_noise_scale
+        noise_high = self._reset_noise_scale
+
+        qpos = self.init_qpos + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nq
+        )
+        qvel = (
+            self.init_qvel
+            + self._reset_noise_scale * self.np_random.standard_normal(self.model.nv)
+        )
+
+        self.set_state(qpos, qvel)
+
+        observation = self._get_obs()
+        return observation
+
+    def viewer_setup(self):
+        assert self.viewer is not None
+        for key, value in DEFAULT_CAMERA_CONFIG.items():
+            if isinstance(value, np.ndarray):
+                getattr(self.viewer.cam, key)[:] = value
+            else:
+                setattr(self.viewer.cam, key, value)
+
+    # Expose a model-based reward function for planning (torch tensors expected)
+    def get_model_reward_fn(self):
+        dt = float(self.dt)
+        forward_reward_weight = float(getattr(self, "_forward_reward_weight", 1.0))
+        ctrl_cost_weight = float(getattr(self, "_ctrl_cost_weight", 0.05))
+        return HalfCheetahRewardFn(dt, forward_reward_weight, ctrl_cost_weight)
+
+    # HalfCheetah does not terminate early in our variant
+    def get_model_termination_fn(self):
+        return HalfCheetahTerminationFn()
