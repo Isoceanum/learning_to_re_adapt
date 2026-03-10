86b6896224de4e91333549585bde31a48b898861
Isoceanum <Isoceanum@outlook.com>
2026-03-10 17:45:20 +0100

    new sweeo

--- algorithms/mb_mpc/planner.py
+++ algorithms/mb_mpc/planner.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class RandomShootingPlanner:
     def __init__(self, dynamics_fn, reward_fn, horizon, n_candidates, act_low, act_high, device, discount=1.0):
-        self.dynamics_fn = dynamics_fn # function used to predict next state from current state and action
+        self.dynamics_fn = torch.compile(dynamics_fn) # function used to predict next state from current state and action
         self.reward_fn = reward_fn # model based reward function that computes reward using single transition (state, action, next_state)
         self.horizon = horizon # number of steps to plan ahead
         self.n_candidates = n_candidates # number of random action sequences to test
@@ -15,7 +15,7 @@ class RandomShootingPlanner:
         
         self.discount = discount # discount factor (gamma) for future rewards.
     
-    @torch.no_grad()
+    @torch.inference_mode()
     def plan(self, state):
         # convert numpy state to a torch tensor if not already a tensor
         if isinstance(state, np.ndarray): state = torch.as_tensor(state)  
@@ -53,7 +53,7 @@ class RandomShootingPlanner:
 
 class CrossEntropyMethodPlanner:
     def __init__(self, dynamics_fn, reward_fn, horizon, n_candidates, act_low, act_high, device, discount, num_cem_iters, percent_elites, alpha, seed):
-        self.dynamics_fn = dynamics_fn
+        self.dynamics_fn = torch.compile(dynamics_fn)
         self.reward_fn = reward_fn
         self.horizon = int(horizon)
         self.n_candidates = int(n_candidates)
@@ -72,7 +72,7 @@ class CrossEntropyMethodPlanner:
         self.gen = torch.Generator(device=self.device)
         self.gen.manual_seed(int(seed))
 
-    @torch.no_grad()
+    @torch.inference_mode()
     def plan(self, state):
         # Match RS: state is a single observation vector (obs_dim,)
         if isinstance(state, np.ndarray):
